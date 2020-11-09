c994353e63093e4c6af65be378b7756b4191388c
vilmanyiadrian <vilmanyi.adrian@gmail.com>
2020-11-09 16:15:32 +0100

    check

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const Rank = () => {
+	return ( 
+<div>
+	<div className='f3 white center'>
+		{'Your current rank is:'}
+	</div>
+	<div className='f1 white center'>
+		{'#2'}
+	</div>
+	
+</div>
+
+		);
+}
+
+export default Rank;

bacfac36122bddfdc13a1217ac1f43b2a59d8588
vilmanyiadrian <vilmanyi.adrian@gmail.com>
2020-11-25 20:45:50 +0100

    rank

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -5,13 +5,18 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({onInputChange, onSubmit}) => {
 	return ( 
 <div>
-<p className='f4 center'>
+<p className='f2 center'>
 {'Face Detector'}
+
 </p>
+
 <div className='center'>
 	<div className='form pa4 br3 shadow-5 center'>
-	<input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
-	<button className='w-30 grow f4 link ph3 pv2 dib white' onClick={onSubmit} style={{background: 'orange'}}>Detect</button>
+	<p className='f6 pr4'>URL</p>
+	<input className='f3 w-70 center' type='tex' onChange={onInputChange}/>
+
+	<button className='w-30 grow f3 link ph3 pv2 dib white' onClick={onSubmit} style={{background: 'orange'}}>Detect</button>
+
 	</div>
 </div>
 </div>
--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -5,12 +5,7 @@ import React from 'react';
 const Rank = () => {
 	return ( 
 <div>
-	<div className='f3 white center'>
-		{'Your current rank is:'}
-	</div>
-	<div className='f4 white center'>
-		{'#2'}
-	</div>
+
 	
 </div>
 
